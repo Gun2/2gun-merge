2dd0cc31d01adbc015ccb5a29f3ac4eb9e20d605
gun2 <0dayz@naver.com>
2022-11-24 01:17:03 +0900

    spring boot init

---
+++ src/main/java/com/github/merge2gun/Merge2gunApplication.java
@@ -0,0 +1,13 @@
+package com.github.merge2gun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Merge2gunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Merge2gunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/merge2gun/Merge2gunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.merge2gun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Merge2gunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
